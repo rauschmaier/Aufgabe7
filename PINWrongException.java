48cbb4cb47413f8901a2b5b0fc4f92c33682ebb6
rauschmaier <95904777+rauschmaier@users.noreply.github.com>
2023-12-04 20:24:09 +0100

    Update PINWrongException.java

--- PINWrongException.java
+++ PINWrongException.java
@@ -1,13 +1,5 @@
 package pinabfrage;
 
 public class PINWrongException extends Exception {
-	private String errorMessage;
-	
-	public PINWrongException(String errorMessage) {
-		this.errorMessage = errorMessage;
-	}
-	
-	public String getErrorMessage() {
-		return errorMessage;
-	}
+
 }
